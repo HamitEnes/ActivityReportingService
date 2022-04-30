6fd5b508e02963bef44401656579655c4e47b3d3
Hamit Enes Gümüş <hamitenes@hotmail.com>
2022-04-30 16:10:34 +0300

    Added activity model class with initial structure.

---
+++ ActivityReportingService/Models/ActivityManagement/Activity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ActivityReportingService.Models.ActivityManagement
+{
+    /// <summary>
+    /// Model class for storing activity data
+    /// </summary>
+    public class Activity
+    {
+        /// <summary>
+        /// Auto incremented key value
+        /// </summary>
+        public int ActivityId { get; set; }
+
+        /// <summary>
+        /// Requesting activity name as a parameter of API and storing activities by name.
+        /// This will be used for generating totals by activity.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// For pruning greater than 12 hour data. This property will be used.
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Value of the activity.
+        /// </summary>
+        public int Value { get; set; }
+    }
+}
