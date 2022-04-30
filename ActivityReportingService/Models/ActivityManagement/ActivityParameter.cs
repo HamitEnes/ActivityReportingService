c35869b2e0e0a57cd19b309cd1c5bb696215bc79
Hamit Enes Gümüş <hamitenes@hotmail.com>
2022-04-30 17:16:14 +0300

    Added activity post operation. And ActivityParameter class

---
+++ ActivityReportingService/Models/ActivityManagement/ActivityParameter.cs
@@ -0,0 +1,13 @@
+﻿namespace ActivityReportingService.Models.ActivityManagement
+{
+    /// <summary>
+    /// In Site Activity Reporting Service documentation. There is a little info about that,
+    /// Store the activity value as an integer (round to the nearest number).
+    /// So, POST requested value data seems may be out of integer data. This class is using
+    /// as a parameter of Activity POST requests. With this way endpoint can get the value data with precision.
+    /// </summary>
+    public class ActivityParameter
+    {
+        public decimal Value { get; set; }
+    }
+}
